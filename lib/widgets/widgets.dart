bd47aaa52321fe1f0f67dade08e1863082282332
Mustafa <18701031@mersin.edu.tr>
2020-07-19 23:25:57 +0300

    Sign In & Sign Up with Firebase

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -13,3 +13,17 @@ Widget appBar(BuildContext context){
     ),
   );
 }
+
+Widget blueButton(BuildContext context, String label){
+  return Container(
+                    padding: EdgeInsets.symmetric(vertical : 18),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(30)
+                      ),
+                      height: 55,
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 48,
+                      child: Text(label, style: TextStyle(color: Colors.white, fontSize: 16),),
+                    );
+}
