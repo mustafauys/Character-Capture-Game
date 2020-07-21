9fbea2828cef860bbced299bbab094d39a148cbd
Mustafa <18701031@mersin.edu.tr>
2020-07-21 04:51:06 +0300

    Editing Done

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -6,32 +6,30 @@ import 'package:knowledge_treasure/widgets/widgets.dart';
 
 class SignIn extends StatefulWidget {
   @override
-   SignInState createState() =>  SignInState();
+  SignInState createState() => SignInState();
 }
 
 class SignInState extends State<SignIn> {
-
   final _formKey = GlobalKey<FormState>();
   String email, password;
   AuthService authService = new AuthService();
 
   bool _isLoading = false;
 
-  signIn() async{
-    if (_formKey.currentState.validate()){
+  signIn() async {
+    if (_formKey.currentState.validate()) {
       setState(() {
         _isLoading = true;
       });
       await authService.signInEmailAndPass(email, password).then((val) {
         if (val != null) {
           setState(() {
-        _isLoading = false;
-      });
-        Navigator.pushReplacement(context, MaterialPageRoute(
-        builder: (context) => Home()
-      ));
+            _isLoading = false;
+          });
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => Home()));
         }
-      }); 
+      });
     }
   }
 
@@ -44,69 +42,82 @@ class SignInState extends State<SignIn> {
         elevation: 0.0,
         brightness: Brightness.light,
       ),
-      body: _isLoading ? Container(
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
-      ) : Form(
-        key: _formKey,
+      body: _isLoading
+          ? Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : Form(
+              key: _formKey,
               child: Container(
                 margin: EdgeInsets.symmetric(horizontal: 24),
-                
-          child: Column(
-            children: <Widget>[
-              Spacer(),
-                TextFormField(
-                  validator: (val){return val.isEmpty ? "Enter Email id" : null;},
-                  decoration: InputDecoration(
-                    hintText: "Email"
-                  ),
-                  onChanged: (val){
-                    email = val;
-                  },
-                ),
-                SizedBox(height: 6,),
-
-                TextFormField(
-                  obscureText: true,
-                  validator: (val){return val.isEmpty ? "Enter Password" : null;},
-                  decoration: InputDecoration(
-                    hintText: "Password"
-                  ),
-                  onChanged: (val){
-                    password = val;
-                  },
-                ),
-                SizedBox(height: 24,),
-                  GestureDetector(
-                    onTap: () {
-                      signIn();
-                    },
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    TextFormField(
+                      validator: (val) {
+                        return val.isEmpty ? "Enter Email id" : null;
+                      },
+                      decoration: InputDecoration(hintText: "Email"),
+                      onChanged: (val) {
+                        email = val;
+                      },
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      validator: (val) {
+                        return val.isEmpty ? "Enter Password" : null;
+                      },
+                      decoration: InputDecoration(hintText: "Password"),
+                      onChanged: (val) {
+                        password = val;
+                      },
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        signIn();
+                      },
                       child: blueButton(context, "Sign In"),
-                  ),
-                  SizedBox(height: 18,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Don't have an account? ", style: TextStyle(fontSize: 15.5),),
-                      GestureDetector(
-                        onTap: (){
-                          Navigator.pushReplacement(context, MaterialPageRoute(
-                            builder: (context) => SignUp()
-                          ));
-                          
-                        },
-                        child: Text("Sign Up",style: TextStyle(fontSize: 15.5, decoration: TextDecoration.underline),)),
-                    ],
-                  ),
-
-
-                SizedBox(height: 80,),
-            ],
-          ),
-        ),
-      ),
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Don't have an account? ",
+                          style: TextStyle(fontSize: 15.5),
+                        ),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignUp()));
+                            },
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                  fontSize: 15.5,
+                                  decoration: TextDecoration.underline),
+                            )),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 80,
+                    ),
+                  ],
+                ),
+              ),
+            ),
     );
   }
-  
 }
