59f17fa0547d5701cf83b77ffcb47c512efd6ff6
Mustafa <18701031@mersin.edu.tr>
2020-07-21 04:50:56 +0300

    Editing Done

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
-Widget appBar(BuildContext context){
+Widget appBar(BuildContext context) {
   return Center(
     child: RichText(
       text: TextSpan(
         style: TextStyle(fontSize: 22),
-        children: <TextSpan> [
-          TextSpan(text: 'Quiz', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.black54)),
-          TextSpan(text: 'Treasure', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.blue)),
+        children: <TextSpan>[
+          TextSpan(
+              text: 'Quiz',
+              style: TextStyle(
+                  fontWeight: FontWeight.w600, color: Colors.black54)),
+          TextSpan(
+              text: 'Treasure',
+              style:
+                  TextStyle(fontWeight: FontWeight.w600, color: Colors.blue)),
         ],
       ),
     ),
   );
 }
 
-Widget blueButton(BuildContext context, String label){
+Widget blueButton(BuildContext context, String label) {
   return Container(
-                    padding: EdgeInsets.symmetric(vertical : 18),
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(30)
-                      ),
-                      height: 55,
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width - 48,
-                      child: Text(label, style: TextStyle(color: Colors.white, fontSize: 16),),
-                    );
+    padding: EdgeInsets.symmetric(vertical: 18),
+    decoration: BoxDecoration(
+        color: Colors.blue, borderRadius: BorderRadius.circular(30)),
+    height: 55,
+    alignment: Alignment.center,
+    width: MediaQuery.of(context).size.width - 48,
+    child: Text(
+      label,
+      style: TextStyle(color: Colors.white, fontSize: 16),
+    ),
+  );
 }
