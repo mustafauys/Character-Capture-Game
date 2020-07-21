4eec5cf33800e3ae1164b045c01c0b2e57f0e4d8
Mustafa <18701031@mersin.edu.tr>
2020-07-21 04:50:47 +0300

    Editing Done

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,13 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-  Future<void> addQuizData()
+  Future<void> addQuizData(Map quizData, String quizId) async {
+    await Firestore.instance
+        .collection("Quiz")
+        .document(quizId)
+        .setData(quizData)
+        .catchError((e) {
+      print(e.toString());
+    });
+  }
 }
