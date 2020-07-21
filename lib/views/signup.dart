ed9ced939a851ee0c4c7ec105b3794ef0a909f7e
Mustafa <18701031@mersin.edu.tr>
2020-07-21 04:51:01 +0300

    Editing Done

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -10,25 +10,26 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-
   final _formKey = GlobalKey<FormState>();
-  String email, password,name;
+  String email, password, name;
   AuthService authService = new AuthService();
-  bool _isLoading= false;
+  bool _isLoading = false;
 
   signUp() async {
-    if(_formKey.currentState.validate()){
+    if (_formKey.currentState.validate()) {
       setState(() {
         _isLoading = true;
       });
-      authService.signUpWithEmailAndPassword(email, password).then((value){
+      authService.signUpWithEmailAndPassword(email, password).then((value) {
         if (value != null) {
           setState(() {
-        _isLoading = false;
-      });
-          Navigator.pushReplacement(context, MaterialPageRoute (
-            builder: (context) => Home(),
-          ));
+            _isLoading = false;
+          });
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Home(),
+              ));
         }
       });
     }
@@ -43,77 +44,94 @@ class _SignUpState extends State<SignUp> {
         elevation: 0.0,
         brightness: Brightness.light,
       ),
-      body: _isLoading ? Container (
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
-      ) : Form(
-        key: _formKey,
+      body: _isLoading
+          ? Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          : Form(
+              key: _formKey,
               child: Container(
                 margin: EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            children: <Widget>[
-              Spacer(),
-              TextFormField(
-                  validator: (val){return val.isEmpty ? "Enter Name" : null;},
-                  decoration: InputDecoration(
-                    hintText: "Name"
-                  ),
-                  onChanged: (val){
-                    name = val;
-                  },
-                ),
-                SizedBox(height: 6,),
-                TextFormField(
-                  validator: (val){return val.isEmpty ? "Enter Email id" : null;},
-                  decoration: InputDecoration(
-                    hintText: "Email"
-                  ),
-                  onChanged: (val){
-                    email = val;
-                  },
-                ),
-                SizedBox(height: 6,),
-
-                TextFormField(
-                  obscureText: true,
-                  validator: (val){return val.isEmpty ? "Enter Password" : null;},
-                  decoration: InputDecoration(
-                    hintText: "Password"
-                  ),
-                  onChanged: (val){
-                    password = val;
-                  },
-                ),
-                SizedBox(height: 24,),
-                  GestureDetector(
-                    onTap: () {
-                      signUp();
-                    },
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    TextFormField(
+                      validator: (val) {
+                        return val.isEmpty ? "Enter Name" : null;
+                      },
+                      decoration: InputDecoration(hintText: "Name"),
+                      onChanged: (val) {
+                        name = val;
+                      },
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    TextFormField(
+                      validator: (val) {
+                        return val.isEmpty ? "Enter Email id" : null;
+                      },
+                      decoration: InputDecoration(hintText: "Email"),
+                      onChanged: (val) {
+                        email = val;
+                      },
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      validator: (val) {
+                        return val.isEmpty ? "Enter Password" : null;
+                      },
+                      decoration: InputDecoration(hintText: "Password"),
+                      onChanged: (val) {
+                        password = val;
+                      },
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        signUp();
+                      },
                       child: blueButton(context, "Sign Up"),
-                  ),
-                  SizedBox(height: 18,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Alread have an account? ", style: TextStyle(fontSize: 15.5),),
-                      GestureDetector(
-                        onTap: (){
-                          Navigator.pushReplacement(context, MaterialPageRoute(
-                            builder: (context) => SignIn()
-                          ));
-                          
-                        },
-                        child: Text("Sign In",style: TextStyle(fontSize: 15.5, decoration: TextDecoration.underline),)),
-                    ],
-                  ),
-
-
-                SizedBox(height: 80,),
-            ],
-          ),
-        ),
-      ),
+                    ),
+                    SizedBox(
+                      height: 18,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Alread have an account? ",
+                          style: TextStyle(fontSize: 15.5),
+                        ),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignIn()));
+                            },
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  fontSize: 15.5,
+                                  decoration: TextDecoration.underline),
+                            )),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 80,
+                    ),
+                  ],
+                ),
+              ),
+            ),
     );
   }
 }
