fe39770fff7ca2c14ce719aa197309fc2c5f9cd6
Mustafa <18701031@mersin.edu.tr>
2020-07-22 22:02:39 +0300

    Question Screen Page Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:knowledge_treasure/helper/functions.dart';
+import 'package:knowledge_treasure/views/home.dart';
 import 'package:knowledge_treasure/views/signin.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool isUserLoggedIn = false;
+
+  @override
+  void initState() {
+    checkUserLoggedInStatus();
+    super.initState();
+  }
+
+  checkUserLoggedInStatus() async {
+    HelperFunctions.getUserLoggedInDetails().then((value){
+      setState(() {
+        isUserLoggedIn = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +39,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SignIn(),
+      home: (isUserLoggedIn ?? false) ? Home() : SignIn(),
     );
   }
 }
